b1ab3180bd3046c22f302a0e3ad4a23c90479363
mAyman93 <moh.ayman.93@gmail.com>
2020-05-03 13:52:54 +0200

    change in app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
 
-import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
@@ -52,15 +51,12 @@ def predict():
         """
     # Logging the input payload
     json_payload = request.json
-    LOG.info(f"JSON payload: \n{json_payload}")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
-    # scale the input
-    scaled_payload = scale(inference_payload)
+    LOG.info("JSON payload: \n %s", json_payload)
     # get an output prediction from the pretrained model, clf
-    prediction = list(clf.predict(scaled_payload))
+    prediction = list(clf.predict(json_payload))
     # TO DO:  Log the output prediction value
-    return jsonify({'Prediction': prediction})
+    LOG.info("Predicted value: %s", prediction)
+    return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     # load pretrained model as clf
